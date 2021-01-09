9b4e5b5e14d7fbbf76d14902bcf1b6224aba71e1
nexusipx <77172482+nexusipx@users.noreply.github.com>
2021-01-09 13:55:35 +0000

    Ticker Links

--- addclass.js
+++ addclass.js
@@ -12,5 +12,17 @@ function addStyledTableClassToTable() {
     }
 };
 
+
+function addTickerLinks(){
+	var table = document.getElementsByTagName("table")[0];
+	
+    for(var i = 1; i < table.rows.length; i++) {
+		var firstCol = table.rows[i].cells[0]; 
+		var url = 'https://finance.yahoo.com/quote/'+firstCol.innerHTML.trim();
+        firstCol.innerHTML = "<a href='"+url+"'/>"+firstCol.innerHTML.toString()+"</a>";
+    }
+}
+
 addSortableClassToTable();
 addStyledTableClassToTable();
+addTickerLinks();
